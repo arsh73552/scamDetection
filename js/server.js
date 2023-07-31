755518fe52c45b0b3b36b69f1643b58a17281cfa
mandeep-mahra <mandeepmehara1@gmail.com>
2023-08-01 00:32:11 +0530

    api works!!

--- js/server.js
+++ js/server.js
@@ -6,13 +6,9 @@ const helpers = require('./helper');
 app.use(cors());
 app.use(express.json());
 
-app.post('/upload', function (request, response) 
-{
-    async ()=>{
-        const check = await helpers.testIfWebsitePhishy(request.body.websiteURL)
-        res.json({'check': check})
-    }
-    console.log(request.body.websiteURL);
+app.post('/upload', async(request, response) => {
+    const check = await helpers.testIfWebsitePhishy(request.body.websiteURL)
+    response.json({'phishingWebsite' : check});
 });
  
 app.listen(8080);
